99f3c2130ad6fe34030c6a5674c0eefedb805911
Valentin | Electricien <74057469+Electricien50@users.noreply.github.com>
2024-04-18 14:47:27 +0200

    update

--- Autre/Bdd/scripts/1.InsertDefaultTypes.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `CSGOTournamenentMaker`.`T_Type`
-(`idType`, `nomType`)
-VALUES
-(NULL, "2vs2"); 
-
-INSERT INTO `CSGOTournamenentMaker`.`T_Type`
-(`idType`, `nomType`)
-VALUES
-(NULL, "5vs5");
